032aa27c50ccc8beaff39d2a03ec04ce26e0bed8
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-04-20 14:10:50 +0200

    Escape the labyrinth game created and some assets

---
+++ Scripts/Escape the labyrinth/character_labyrinth.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D 
+
+@export var speed = 200
+var has_won = false
+
+func _physics_process(delta):
+	if has_won:
+		velocity = Vector2.ZERO
+		return
+
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
+	move_and_slide()
+
+	if direction != Vector2.ZERO:
+		$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.play("idle")
+
+func _on_area_2d_area_entered(area):
+	if area.name == "Goal":
+		has_won = true
+		$AnimatedSprite2D.play("win")
+		$AudioStreamPlayer2D.play()
