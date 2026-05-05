94dbdb49df51602fd69749ee5a7eeee83335a0ad
aasujape-cmyk <aasujape@gmail.com>
2026-05-05 14:56:14 +0200

    endlessbutton

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -3,13 +3,18 @@ extends Control
 @onready var img_normal = $BackgroundNormal
 @onready var img_victoria = $BackgroundVictory
 
+@onready var endlessbuttonInactive=$EndlessMode
+@onready var endlessbuttonactive=$EndlessModeInactive
 func _ready():
 	if Global.historia_completada:
 		img_normal.hide()
 		img_victoria.show()
+		endlessbuttonactive.visible=false
 	else:
 		img_normal.show()
 		img_victoria.hide()
+		endlessbuttonactive.visible=true
+		endlessbuttonInactive.visible=false
 
 func _on_boton_jugar_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
