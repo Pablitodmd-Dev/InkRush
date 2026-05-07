64e8723e8152a48ec540915a9ee13baf8507e717
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-05-07 10:46:17 +0200

    History Mode Animation Bug fixed

--- Scenes/Animations/HistoryModeAnimation.gd
+++ Scenes/Animations/HistoryModeAnimation.gd
@@ -3,6 +3,18 @@ extends Control
 func _ready() -> void:
 	$AnimationPlayer.play("intro")
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if Global.intro_visto:
+			_iniciar_transicion_final()
+
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "intro": 
-		get_tree().change_scene_to_file("res://Scenes//Main.tscn")
+		Global.intro_visto = true
+		_iniciar_transicion_final()
+	elif anim_name == "starting_game":
+		get_tree().change_scene_to_file("res://Scenes/Main.tscn")
+
+func _iniciar_transicion_final() -> void:
+	$AnimationPlayer.stop()
+	$AnimationPlayer.play("starting_game")
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var historia_completada: bool = false
+var intro_visto = false
 var endless_mode:bool=false
 var difficulty_level:int =0
 var coins:int=0
